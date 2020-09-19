d26b8d33ca9b6fc75d6faf1cdb02f3e7a8e18f41
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-09-19 20:05:01 +0100

    working with props & props children

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">
+          My Hobbies: Racing
+        </Person>
+        <Person name="Stephanie" age="26" />
       </div>
     );
   }
--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const person = () => {
+const person = (props) => {
+  // props.children outputs everything between the opening/closing tags
   return (
-    <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    <div>
+      <p>
+        I'm a {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
