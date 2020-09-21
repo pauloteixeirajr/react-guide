91588041705e4f0e4a05065383dd0cb4af12c970
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-09-21 21:35:00 +0100

    adding two way binding

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -21,6 +21,16 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29, hobbies: 'Racing' },
+        { name: 'Stephanie', age: 26 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -36,6 +46,7 @@ class App extends Component {
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[2].name}
--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -8,6 +8,7 @@ const person = (props) => {
         I'm a {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} defaultValue={props.name} />
     </div>
   );
 };
