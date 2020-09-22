026f2a5943c70bdebced00af8b8a0281cc5e7f36
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-09-22 20:30:38 +0100

    manage lists state

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -12,16 +12,6 @@ class App extends Component {
     showPersons: false,
   };
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29, hobbies: 'Racing' },
-        { name: 'Stephanie', age: 27 },
-      ],
-    });
-  };
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -36,6 +26,12 @@ class App extends Component {
     this.setState({
       showPersons: !this.state.showPersons,
     });
+  };
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons });
   };
 
   render() {
@@ -53,8 +49,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person) => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={this.deletePersonHandler.bind(this, index)}
+                name={person.name}
+                age={person.age}
+              />
+            );
           })}
         </div>
       );
