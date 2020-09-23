d491b973c52944db7858fefbabd24fbc80657c16
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-09-23 19:46:26 +0100

    styled components & dynamic styles

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: ${(props) => (props.show ? 'red' : 'green')};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  border-radius: 3px;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: ${(props) => (props.show ? 'salmon' : 'lightgreen')};
+    color: black;
+  },
+`;
 
 class App extends Component {
   state = {
@@ -39,20 +55,6 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      borderRadius: '3px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black',
-      },
-    };
-
     let persons = null;
 
     if (this.state.showPersons) {
@@ -71,11 +73,6 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black',
-      };
     }
 
     const classes = [];
@@ -91,9 +88,12 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>Click the button to toggle persons</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <StyledButton
+          show={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
           Toggle Persons
-        </button>
+        </StyledButton>
         {persons}
       </div>
     );
