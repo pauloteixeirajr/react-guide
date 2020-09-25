28258ed1b9905f3a1c71b34526d5ed5afbaf5de8
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-09-25 19:06:52 +0100

    using refs to reference elements

--- react-guide/src/components/Persons/Person/Person.js
+++ react-guide/src/components/Persons/Person/Person.js
@@ -12,7 +12,14 @@ const person = (props) => {
         I'm a {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
+      <input
+        ref={(inputEl) => {
+          inputEl.focus();
+        }}
+        type="text"
+        onChange={props.changed}
+        value={props.name}
+      />
     </div>
   );
 };
