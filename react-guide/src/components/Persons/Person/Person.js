892b8ea2ad6af607ea15f4755ce3d63448433caa
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-09-25 18:23:09 +0100

    using React.Fragment as an HOC component

--- react-guide/src/components/Persons/Person/Person.js
+++ react-guide/src/components/Persons/Person/Person.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
   // props.children outputs everything between the opening/closing tags
   return (
-    <div className={classes.Person}>
+    <Fragment>
       <p onClick={props.click}>
         I'm a {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+    </Fragment>
   );
 };
 
