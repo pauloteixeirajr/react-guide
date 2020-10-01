ad3e06f5ee2c935e3e42ce631f8d05c846be3db6
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-10-01 21:40:38 +0100

    reusing the backdrop

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,31 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 import classes from './SideDrawer.module.css';
 
 const sideDrawer = (props) => {
+  const attachedClasses = [classes.SideDrawer];
+  if (props.open) {
+    attachedClasses.push(classes.Open);
+  } else {
+    attachedClasses.push(classes.Close);
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop clicked={props.closed} show={props.open} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems></NavigationItems>
+        </nav>
       </div>
-      <nav>
-        <NavigationItems></NavigationItems>
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
