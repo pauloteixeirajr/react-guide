b5d020495144f880e59dc465c3224ffa8a17d2d3
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-10-07 20:07:48 +0100

    preparing the project for routing

--- blog-posts/src/containers/Blog/Blog.js
+++ blog-posts/src/containers/Blog/Blog.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+
+import Posts from '../Posts/Posts';
 
 class Blog extends Component {
   state = {
@@ -12,37 +9,7 @@ class Blog extends Component {
     selectedPost: null,
   };
 
-  componentDidMount() {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return {
-            ...post,
-            author: 'Paulo',
-          };
-        });
-        this.setState({ posts: updatedPosts });
-      })
-      .catch(console.log);
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPost: id });
-  };
-
   render() {
-    const posts = this.state.posts.map((post) => {
-      return (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
-      );
-    });
     return (
       <div className="Blog">
         <header>
@@ -57,13 +24,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPost} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+        <Posts />
       </div>
     );
   }
---
+++ blog-posts/src/containers/Posts/Posts.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Post from '../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+  state = {
+    posts: [],
+    selectedPost: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: 'Paulo',
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(console.log);
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPost: id });
+  };
+
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
+    });
+    return <section className="Posts">{posts}</section>;
+  }
+}
+
+export default Posts;
