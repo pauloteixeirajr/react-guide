6e8adabc15c5c8bd84b6b3722ad53a79058e3272
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-10-12 19:45:30 +0100

    building the checkout container

---
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+import CheckOutSummary from '../../components/Order/Order';
+
+class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <CheckOutSummary ingredients={this.state.ingredients} />
+      </div>
+    );
+  }
+}
+
+export default Checkout;
