45d5b22263c188f0fa844082501e6fb58c721235
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-10-12 21:57:58 +0100

    adding an orders page

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import CheckOutSummary from '../../components/Order/Order';
+import CheckOutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
