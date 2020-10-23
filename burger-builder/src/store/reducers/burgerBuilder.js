10317e9cd0c30e7ae9726fef6b3bf3b98105d44f
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-10-23 21:02:59 +0100

    resetting the price after purchases

--- burger-builder/src/store/reducers/burgerBuilder.js
+++ burger-builder/src/store/reducers/burgerBuilder.js
@@ -40,6 +40,7 @@ const reducer = (state = initialState, action) => {
           ...action.ingredients,
         },
         error: false,
+        totalPrice: 4,
       };
     case actionTypes.FETCH_INGREDIENTS_FAILED:
       return {
