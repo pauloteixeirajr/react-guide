cf1622ab47c0ede8bd1e7c7da80e7225114a4254
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-11-02 20:54:40 +0000

    nextjs navigating basics

--- next.js/pages/auth/index.js
+++ next.js/pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Router from 'next/router';
 
 const authIndexPage = () => (
   <div>
     <h1>The Auth Index Page</h1>
+    <button onClick={() => Router.push('/')}>Go back to Home</button>
   </div>
 );
 
--- next.js/pages/index.js
+++ next.js/pages/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Link from 'next/link';
 
 const indexPage = () => (
   <div>
     <h1>The Main Page</h1>
+    <p>
+      Go to <Link href="/auth">Auth</Link>
+    </p>
   </div>
 );
 
