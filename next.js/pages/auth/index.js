15600e9a91cd16540aa8a2a9d2241bc04613d0aa
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-11-02 20:58:33 +0000

    using components with nextjs

--- next.js/pages/auth/index.js
+++ next.js/pages/auth/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Router from 'next/router';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
   <div>
     <h1>The Auth Index Page</h1>
+    <User name="Paulo" age={31} />
     <button onClick={() => Router.push('/')}>Go back to Home</button>
   </div>
 );
