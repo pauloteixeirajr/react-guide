115bbfe460f4db144e2a2296cd0c013f8f2ca4d5
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-11-02 21:04:41 +0000

    handling 404 errors

---
+++ next.js/pages/_error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const errorPage = () => (
+  <div>
+    <h1>Ops, something went wrong</h1>
+    <p>
+      Try <Link href="/">going back</Link>.
+    </p>
+  </div>
+);
+
+export default errorPage;
