5a6bec947ef20018ba6982c48427f6c62e260443
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-11-03 19:24:34 +0000

    using the transition component to animate the modal

--- animations/src/App.js
+++ animations/src/App.js
@@ -9,7 +9,6 @@ import List from './components/List/List';
 class App extends Component {
   state = {
     modalIsOpen: false,
-    showBlock: false,
   };
 
   showModal = () => {
@@ -20,40 +19,18 @@ class App extends Component {
     this.setState({ modalIsOpen: false });
   };
 
-  toggleBlock = () => {
-    this.setState((prevState) => {
-      return { showBlock: !prevState.showBlock };
-    });
-  };
-
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <button className="Button" onClick={this.toggleBlock}>
-          Toggle
-        </button>
-        <br />
         <Transition
           mountOnEnter
           unmountOnExit
-          in={this.state.showBlock}
-          timeout={1000}
+          in={this.state.modalIsOpen}
+          timeout={300}
         >
-          {(state) => (
-            <div
-              style={{
-                backgroundColor: 'red',
-                width: 100,
-                height: 100,
-                margin: 'auto',
-                transition: 'all 1s ease-out',
-                opacity: state === 'exiting' ? 0 : 1,
-              }}
-            ></div>
-          )}
+          {(state) => <Modal show={state} closed={this.closeModal} />}
         </Transition>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen} />
         <button onClick={this.showModal} className="Button">
           Open Modal
