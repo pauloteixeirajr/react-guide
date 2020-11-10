7a9036af3fed82911c5372a9de24e18551639606
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-11-10 19:24:16 +0000

    using custom hook as a store

---
+++ replace-redux/src/hooks/store.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+let actions = [];
+let globalState = {};
+let listeners = [];
+
+const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (action) => {
+    const newState = actions[action](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    listeners.push(setState);
+
+    return () => {
+      listeners = listeners.filter((li) => li !== setState);
+    };
+  }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
+
+export default useStore;
