e02cbe7740e64e0a40b71291c27d4f2667d50e8b
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-05-24 22:13:46 +0100

    creating expense item component

--- expense/src/App.jsx
+++ expense/src/App.jsx
@@ -1,9 +1,12 @@
+import ExpenseItem from './components/ExpenseItem/ExpenseItem';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's get started</h1>
+      <ExpenseItem />
     </div>
   );
 }
