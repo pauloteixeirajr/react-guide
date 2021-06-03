29fb7e7df86c1576232f2ea5714db3949a8ce40a
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-06-03 22:09:36 +0100

    using useEffect hook

--- advanced/src/App.js
+++ advanced/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,10 +6,16 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const storeUserLoggedIn = localStorage.getItem('isLoggedIn');
+
+  useEffect(() => {
+    setIsLoggedIn(storeUserLoggedIn === '1');
+  }, [storeUserLoggedIn]);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', 1);
     setIsLoggedIn(true);
   };
 
