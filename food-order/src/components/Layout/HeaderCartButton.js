8b857682283d5005f0f14480a031bd3c4ad8ee38
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-06-16 22:56:56 +0100

    animating the cart button with useEffect

--- food-order/src/components/Layout/HeaderCartButton.js
+++ food-order/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartContext from '../../store/cart-context';
 
@@ -7,14 +7,27 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+  const [btnAnimation, setBtnAnimation] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((total, cur) => {
     return total + cur.amount;
   }, 0);
 
+  const buttonClasses = `${classes.button} ${btnAnimation ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (!cartCtx.items.length) return;
+    setBtnAnimation(true);
+    const timeout = setTimeout(() => setBtnAnimation(false), 300);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={classes.button} {...props}>
+    <button className={buttonClasses} {...props}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
