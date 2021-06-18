a591ee1d3cd0739f49b5009770efe1eb57b9d574
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-06-18 22:03:20 +0100

    introducing error boundaries

--- class-based-components/src/components/UserFinder.js
+++ class-based-components/src/components/UserFinder.js
@@ -1,5 +1,6 @@
 import { Component, Fragment } from 'react';
 
+import ErrorBoundary from './ErrorBoundary';
 import Users from './Users';
 import classes from './UserFinder.module.css';
 
@@ -44,7 +45,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
