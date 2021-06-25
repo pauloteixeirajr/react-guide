0229d7bb973680b8aec1f46b12a1e55e4563951a
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-06-25 21:48:37 +0100

    getting user input values with useRef

--- react-forms/src/components/SimpleInput.js
+++ react-forms/src/components/SimpleInput.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const SimpleInput = props => {
+  const nameInputRef = useRef();
   const [name, setName] = useState('initialState');
 
   const nameInputChangeHandler = event => {
@@ -9,15 +10,21 @@ const SimpleInput = props => {
 
   const formSubmissionHandler = event => {
     event.preventDefault();
+    const refValue = nameInputRef.current.value;
 
-    console.log(name);
+    console.log(name, refValue);
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
