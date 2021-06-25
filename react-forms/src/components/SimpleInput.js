b843a023fecc5c90fd48e8a986297434b072f328
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-06-25 21:46:53 +0100

    getting user input values with change handlers

--- react-forms/src/components/SimpleInput.js
+++ react-forms/src/components/SimpleInput.js
@@ -1,9 +1,23 @@
-const SimpleInput = (props) => {
+import { useState } from 'react';
+
+const SimpleInput = props => {
+  const [name, setName] = useState('initialState');
+
+  const nameInputChangeHandler = event => {
+    setName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log(name);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
